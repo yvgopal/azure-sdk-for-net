5967f893f229cfcd4e7c8c730c9e5252c9b66342
seanmcc-msft <seanmcc@microsoft.com>
2020-02-28 13:07:31 -0800

    Added basic samples

---
+++ sdk/storage/Azure.Storage.QuickQuery/samples/QuickQuerySample.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Azure.Storage.QuickQuery.Samples
+{
+    public class QuickQuerySample : SampleTest
+    {
+        /// <summary>
+        /// Creates a Stream of a CSV file.
+        /// This is so we have some data to work with.
+        /// </summary>
+        /// <param name="length">The legnth of the stream to create.</param>
+        protected Stream CreateCsvStream(long length)
+        {
+            MemoryStream stream = new MemoryStream();
+            byte[] rowData = Encoding.UTF8.GetBytes("100,200,300,400\n300,400,500,600\n");
+            long blockLength = 0;
+            while (blockLength < length)
+            {
+                stream.Write(rowData, 0, rowData.Length);
+                blockLength += rowData.Length;
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
+        }
+    }
+}
--- sdk/storage/Azure.Storage.QuickQuery/samples/Sample01b_HelloWorldAsync.cs
+++ sdk/storage/Azure.Storage.QuickQuery/samples/Sample01b_HelloWorldAsync.cs
@@ -7,6 +7,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 using NUnit.Framework;
 
 namespace Azure.Storage.QuickQuery.Samples
@@ -14,7 +17,47 @@ namespace Azure.Storage.QuickQuery.Samples
     /// <summary>
     /// Basic Azure Storage Quick Query samples.
     /// </summary>
-    public class Sample01b_HelloWorldAsync : SampleTest
+    public class Sample01b_HelloWorldAsync : QuickQuerySample
     {
+        [Test]
+        public async Task QueryAsync()
+        {
+            // Get a connection string to our Azure Storage account.
+            string connectionString = ConnectionString;
+
+            // Get a reference to a container named "sample-container" and then create it
+            BlobContainerClient container = new BlobContainerClient(connectionString, Randomize("sample-container"));
+            await container.CreateAsync();
+
+            try
+            {
+                // Get a reference to a blob named "sample-file"
+                BlockBlobClient blockBlob = container.GetBlockBlobClient(Randomize("sample-file"));
+
+                // Get CSV stream
+                Stream csvStream = CreateCsvStream(length: 1024);
+
+                // Upload CSV stream
+                await blockBlob.UploadAsync(csvStream);
+
+                // Create BlobQuickQueryClient
+                BlobQuickQueryClient queryClient = blockBlob.GetQuickQueryClient();
+
+                // Create query
+                string query = @"SELECT _2 from BlobStorage WHERE _1 > 250;";
+
+                // Query the service
+                Response<BlobDownloadInfo> response = await queryClient.QueryAsync(query);
+
+                // Read the resulting Stream
+                using StreamReader streamReader = new StreamReader(response.Value.Content);
+                string s = await streamReader.ReadToEndAsync();
+            }
+            finally
+            {
+                // Clean up after the test when we're finished
+                await container.DeleteAsync();
+            }
+        }
     }
 }
